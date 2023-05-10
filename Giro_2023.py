644806585fce1624ba21ffdf4501b22c6fa66ef9
Aaron Lael <aaron.j.lael@gmail.com>
2023-05-10 11:40:11 -0600

    day 5

--- Giro_2023.py
+++ Giro_2023.py
@@ -1,4 +1,4 @@
-from Stage3 import stagewinner, pink, white, blue, purple
+from Stage5 import stagewinner, pink, white, blue, purple
 from friends import kyle, lee, aaron
 
 
--- Stage5.py
+++ Stage5.py
@@ -1,10 +1,369 @@
-stagewinner = ""
-pink = """"""
+stagewinner = "GROVES Kaden"
+pink = """1			NOR	LEKNESSUND Andreas	Team DSM	19:06:03
+2			BEL	EVENEPOEL Remco	Soudal Quick-Step	+ 28
+3			FRA	PARET-PEINTRE Aurélien	AG2R Citroën Team	+ 30
+4			POR	ALMEIDA Joao	UAE Team Emirates	+ 01:00
+5			SLO	ROGLIC Primoz	Jumbo-Visma	+ 01:12
+6			GBR	THOMAS Geraint	INEOS Grenadiers	+ 01:26
+7			RUS	VLASOV Aleksandr	BORA-hansgrohe	+ 01:26
+8			LAT	SKUJINS Toms	Trek-Segafredo	+ 01:29
+9			GBR	GEOGHEGAN HART Tao	INEOS Grenadiers	+ 01:30
+10		 1	ITA	ALBANESE Vincenzo	EOLO-Kometa Cycling Team	+ 01:39
+11		 1	GER	KÄMNA Lennard	BORA-hansgrohe	+ 01:54
+12		 1	ITA	CARUSO Damiano	Bahrain Victorious	+ 01:59
+13		 1	IRL	DUNBAR Edward	Team Jayco-AlUla	+ 02:15
+14		 2	FRA	SIVAKOV Pavel	INEOS Grenadiers	+ 02:23
+15		 3	AUS	HAIG Jack	Bahrain Victorious	+ 02:26
+16		 3	BEL	DE PLUS Laurens	INEOS Grenadiers	+ 02:32
+17		 3	FRA	PINOT Thibaut	Groupama-FDJ	+ 02:33
+18		 3	NED	ARENSMAN Thymen	INEOS Grenadiers	+ 02:33
+19		 3	USA	BARTA William	Movistar Team	+ 02:45
+20		 3	GBR	CARTHY Hugh	EF Education-EasyPost	+ 02:46
+21		 11	AUS	VINE Jay	UAE Team Emirates	+ 02:47
+22		 2	COL	BUITRAGO Santiago	Bahrain Victorious	+ 02:52
+23		 2	ITA	POZZOVIVO Domenico	Israel-Premier Tech	+ 03:01
+24		 7	COL	URAN Rigoberto	EF Education-EasyPost	+ 03:34
+25		 2	COL	RUBIO Einer	Movistar Team	+ 03:35
+26		 5	AUT	KONRAD Patrick	BORA-hansgrohe	+ 04:24
+27		 12	ITA	CONCI Nicola	Alpecin-Deceuninck	+ 04:35
+28			USA	RICCITELLO Matthew	Israel-Premier Tech	+ 04:58
+29			ITA	VELASCO Simone	Astana Qazaqstan Team	+ 05:10
+30			ITA	FORTUNATO Lorenzo	EOLO-Kometa Cycling Team	+ 05:27
+31		 6	FRA	BARGUIL Warren	Team Arkéa-Samsic	+ 05:42
+32			BEL	HUYS Laurens	Intermarché-Circus-Wanty	+ 06:08
+33			ECU	CEPEDA Alexander	EF Education-EasyPost	+ 06:27
+34		 5	ITA	COVILI Luca	Green Project-Bardiani CSF-Faizanè	+ 06:35
+35		 6	BEL	VAN WILDER Ilan	Soudal Quick-Step	+ 06:47
+36		 2	NED	MOLLEMA Bauke	Trek-Segafredo	+ 07:14
+37		 3	ERI	GEBREIGZABHIER Amanuel	Trek-Segafredo	+ 07:25
+38		 2	BEL	VERVAEKE Louis	Soudal Quick-Step	+ 07:41
+39		 1	ITA	PETILLI Simone	Intermarché-Circus-Wanty	+ 07:49
+40		 2	ESP	SÁNCHEZ Luis León	Astana Qazaqstan Team	+ 07:51
+41		 2	NED	VAN DEN BERG Lars	Groupama-FDJ	+ 07:52
+42		 2	ESP	LASTRA Jonathan	Cofidis	+ 07:55
+43		 8	USA	KUSS Sepp	Jumbo-Visma	+ 08:02
+44		 3	GER	HESSMAN Michel	Jumbo-Visma	+ 09:01
+45		 3	SUI	KÜNG Stefan	Groupama-FDJ	+ 09:04
+46		 1	KAZ	PRONSKIY Vadim	Astana Qazaqstan Team	+ 09:20
+47		 21	NED	BOUWMAN Koen	Jumbo-Visma	+ 09:31
+48		 2	ITA	FORMOLO Davide	UAE Team Emirates	+ 09:31
+49		 3	USA	MCNULTY Brandon	UAE Team Emirates	+ 09:47
+50		 4	ITA	TONELLI Alessandro	Green Project-Bardiani CSF-Faizanè	+ 10:11
+51		 2	NED	OOMEN Sam	Jumbo-Visma	+ 10:15
+52		 1	ITA	ZANA Filippo	Team Jayco-AlUla	+ 10:32
+53		 1	ITA	FRIGO Marco	Israel-Premier Tech	+ 10:33
+54		 2	CAN	GEE Derek	Israel-Premier Tech	+ 12:20
+55		 2	IRL	HEALY Ben	EF Education-EasyPost	+ 12:27
+56		 2	ITA	CATTANEO Mattia	Soudal Quick-Step	+ 12:31
+57		 2	ITA	ULISSI Diego	UAE Team Emirates	+ 12:39
+58		 7	GER	DENZ Nico	BORA-hansgrohe	+ 13:06
+59			FRA	ARMIRAIL Bruno	Groupama-FDJ	+ 13:28
+60			NOR	BYSTRØM Sven Erik	Intermarché-Circus-Wanty	+ 14:03
+61			ITA	OLDANI Stefano	Alpecin-Deceuninck	+ 14:08
+62			ITA	MAESTRI Mirco	EOLO-Kometa Cycling Team	+ 14:20
+63			ITA	SCARONI Cristian	Astana Qazaqstan Team	+ 14:22
+64			AUS	MATTHEWS Michael	Team Jayco-AlUla	+ 14:30
+65			ITA	VERRE Alessandro	Team Arkéa-Samsic	+ 14:48
+66		 4	BEL	VANHOUCKE Harm	Team DSM	+ 15:58
+67		 4	FRA	PRODHOMME Nicolas	AG2R Citroën Team	+ 16:07
+68		 4	ITA	ZAMBANINI Edoardo	Bahrain Victorious	+ 16:14
+69		 1	GER	PALZER Anton	BORA-hansgrohe	+ 16:26
+70		 4	DEN	CORT Magnus	EF Education-EasyPost	+ 16:33
+71		 4	ITA	ALEOTTI Giovanni	BORA-hansgrohe	+ 16:40
+72		 3	USA	DOMBROWSKI Joe	Astana Qazaqstan Team	+ 16:50
+73		 5	BEL	REX Laurenz	Intermarché-Circus-Wanty	+ 16:57
+74		 6	FRA	PARET-PEINTRE Valentin	AG2R Citroën Team	+ 17:03
+75		 6	ITA	GAVAZZI Francesco	EOLO-Kometa Cycling Team	+ 17:27
+76		 3	ITA	BATTISTELLA Samuele	Astana Qazaqstan Team	+ 17:35
+77		 10	LUX	JUNGELS Bob	BORA-hansgrohe	+ 17:40
+78		 1	ITA	BAIS Davide	EOLO-Kometa Cycling Team	+ 18:03
+79		 3	ITA	PASQUALON Andrea	Bahrain Victorious	+ 18:09
+80		 4	AUS	GROVES Kaden	Alpecin-Deceuninck	+ 18:23
+81		 5	CZE	HIRT Jan	Soudal Quick-Step	+ 19:07
+82		 4	ITA	MARCELLUSI Martin	Green Project-Bardiani CSF-Faizanè	+ 19:27
+83			ITA	BETTIOL Alberto	EF Education-EasyPost	+ 19:34
+84		 18	ESP	RODRÍGUEZ Óscar	Movistar Team	+ 19:57
+85		 5	ITA	ROTA Lorenzo	Intermarché-Circus-Wanty	+ 20:13
+86		 6	NED	RIESEBEEK Oscar	Alpecin-Deceuninck	+ 20:30
+87			FRA	MOLARD Rudy	Groupama-FDJ	+ 21:18
+88		 1	ECU	CAICEDO Jonathan	EF Education-EasyPost	+ 21:21
+89		 10	AUS	SCOTSON Callum	Team Jayco-AlUla	+ 21:59
+90		 5	EST	TAARAMÄE Rein	Intermarché-Circus-Wanty	+ 22:52
+91		 3	ITA	BAIS Mattia	EOLO-Kometa Cycling Team	+ 23:16
+92		 4	DEN	WÜRTZ SCHMIDT Mads	Israel-Premier Tech	+ 23:44
+93		 2	AUS	BERWICK Sebastian	Israel-Premier Tech	+ 23:58
+94		 2	GBR	STEWART Jake	Groupama-FDJ	+ 24:22
+95		 2	FRA	BOUET Maxime	Team Arkéa-Samsic	+ 24:46
+96		 1	LUX	KIRSCH Alex	Trek-Segafredo	+ 25:28
+97		 1	GER	MAYRHOFER Marius	Team DSM	+ 25:39
+98		 2	DEN	PEDERSEN Mads	Trek-Segafredo	+ 25:52
+99			ITA	VENDRAME Andrea	AG2R Citroën Team	+ 25:54
+100		 1	ERI	MULUBRHAN Henok	Green Project-Bardiani CSF-Faizanè	+ 25:58
+101		 1	RSA	GIBBONS Ryan	UAE Team Emirates	+ 26:23
+102		 7	ITA	DE MARCHI Alessandro	Team Jayco-AlUla	+ 27:25
+103		 5	GBR	SWIFT Ben	INEOS Grenadiers	+ 27:57
+104		 1	AUS	DENNIS Rohan	Jumbo-Visma	+ 28:05
+105		 5	ITA	AFFINI Edoardo	Jumbo-Visma	+ 28:48
+106		 1	ITA	PUCCIO Salvatore	INEOS Grenadiers	+ 28:57
+107		 7	FRA	RUSSO Clément	Team Arkéa-Samsic	+ 29:16
+108		 9	FRA	BAUDIN Alex	AG2R Citroën Team	+ 29:42
+109			GER	STORK Florian	Team DSM	+ 30:10
+110		 10	FRA	GUERNALEC Thibault	Team Arkéa-Samsic	+ 30:43
+111		 5	GER	SÜTTERLIN Jasha	Bahrain Victorious	+ 30:44
+112		 9	GER	MÄRKL Niklas	Team DSM	+ 30:46
+113		 8	ITA	GANNA Filippo	INEOS Grenadiers	+ 31:07
+114		 9	ITA	BONIFAZIO Niccolo	Intermarché-Circus-Wanty	+ 31:12
+115		 9	ITA	DAINESE Alberto	Team DSM	+ 31:13
+116		 2	LUX	RIES Michel	Team Arkéa-Samsic	+ 31:20
+117		 2	RSA	DE BOD Stefan	EF Education-EasyPost	+ 31:39
+118		 8	ESP	SEVILLA Diego	EOLO-Kometa Cycling Team	+ 31:49
+119		 8	ITA	ZOCCARATO Samuele	Green Project-Bardiani CSF-Faizanè	+ 32:03
+120		 8	ITA	GABBURO Davide	Green Project-Bardiani CSF-Faizanè	+ 32:05
+121		 17	GER	ACKERMANN Pascal	UAE Team Emirates	+ 32:15
+122		 9	ERI	TESFAZION Natnael	Trek-Segafredo	+ 32:22
+123		 17	ESP	VERONA Carlos	Movistar Team	+ 32:42
+124		 1	HUN	FETTER Erik	EOLO-Kometa Cycling Team	+ 33:20
+125		 4	BEL	LEYSEN Senne	Alpecin-Deceuninck	+ 33:26
+126		 13	BEL	SERRY Pieter	Soudal Quick-Step	+ 33:30
+127		 5	POL	BENEDETTI Cesare	BORA-hansgrohe	+ 33:37
+128		 17	ESP	ROJAS José Joaquin	Movistar Team	+ 33:50
+129		 5	AUT	PÖSTLBERGER Lukas	Team Jayco-AlUla	+ 35:24
+130		 15	GBR	WILLIAMS Stephen	Israel-Premier Tech	+ 35:45
+131		 6	USA	WARBASSE Lawrence	AG2R Citroën Team	+ 36:18
+132		 1	FRA	CHAMPION Thomas	Cofidis	+ 36:29
+133		 5	FRA	BIDARD François	Cofidis	+ 36:36
+134		 5	ITA	COVI Alessandro	UAE Team Emirates	+ 36:41
+135		 5	AUS	CLARKE Simon	Israel-Premier Tech	+ 37:15
+136		 1	LTU	KONOVALOVAS Ignatas	Groupama-FDJ	+ 38:22
+137		 3	AUS	HEPBURN Michael	Team Jayco-AlUla	+ 39:40
+138		 6	ITA	MAGLI Filippo	Green Project-Bardiani CSF-Faizanè	+ 40:28
+139		 3	ITA	MILAN Jonathan	Bahrain Victorious	+ 41:03
+140		 3	ITA	SBARAGLI Kristian	Alpecin-Deceuninck	+ 41:15
+141		 4	SUI	LIENHARD Fabian	Groupama-FDJ	+ 41:42
+142		 4	GBR	QUARTERMAN Charlie	Team corratec - Selle Italia	+ 41:45
+143		 4	NZL	STEWART Campbell	Team Jayco-AlUla	+ 42:12
+144		 4	ITA	MOSCON Gianni	Astana Qazaqstan Team	+ 42:13
+145		 4	ITA	KONYCHEV Alexander	Team corratec - Selle Italia	+ 42:21
+146		 4	FRA	RIOU Alan	Team Arkéa-Samsic	+ 42:27
+147		 4	ESP	TORRES Albert	Movistar Team	+ 42:34
+148		 4	FRA	TOUMIRE Hugo	Cofidis	+ 42:41
+149		 3	ITA	CONSONNI Simone	Cofidis	+ 42:41
+150		 3	BEL	MARIT Arne	Intermarché-Circus-Wanty	+ 42:42
+151		 4	JPN	ARASHIRO Yukiya	Bahrain Victorious	+ 42:59
+152		 4	FRA	DELETTRE Alexandre	Cofidis	+ 43:02
+153		 5	ITA	FIORELLI Filippo	Green Project-Bardiani CSF-Faizanè	+ 43:11
+154		 5	GBR	CAVENDISH Mark	Astana Qazaqstan Team	+ 43:14
+155		 7	FRA	CHÉREL Mikaël	AG2R Citroën Team	+ 43:21
+156		 7	ITA	BALLERINI Davide	Soudal Quick-Step	+ 43:27
+157		 7	GER	KRIEGER Alexander	Alpecin-Deceuninck	+ 43:27
+158		 7	ITA	CIMOLAI Davide	Cofidis	+ 43:29
+159		 7	NED	DEKKER David	Team Arkéa-Samsic	+ 43:36
+160		 8	ITA	DALLA VALLE Nicolas	Team corratec - Selle Italia	+ 44:25
+161		 7	CZE	VACEK Karel	Team corratec - Selle Italia	+ 44:40
+162		 8	ITA	IACCHI Alessandro	Team corratec - Selle Italia	+ 44:52
+163		 2	NOR	HVIDEBERG Jonas Iversby	Team DSM	+ 45:23
+164		 23	CZE	CERNÝ Josef	Soudal Quick-Step	+ 46:35
+165		 9	NED	HOOLE Daan	Trek-Segafredo	+ 47:44
+166		 1	BEL	VERGAERDE Otto	Trek-Segafredo	+ 48:10
+167		 7	COL	GAVIRIA Fernando	Movistar Team	+ 48:15
+168		 7	GER	KANTER Max	Movistar Team	+ 48:20
+169		 4	NED	TUSVELD Martijn	Team DSM	+ 50:08
+170		 2	SBA	STOJNIC Veljko	Team corratec - Selle Italia	+ 50:49
+171			GBR	GLOAG Thomas	Jumbo-Visma	+ 51:40
+172		 3	ITA	GANDIN Stefano	Team corratec - Selle Italia	+ 52:00"""
 
-white = """"""
+white = """1		NOR	LEKNESSUND Andreas	Team DSM	19:06:03
+2		BEL	EVENEPOEL Remco	Soudal Quick-Step	+ 28
+3		POR	ALMEIDA Joao	UAE Team Emirates	+ 01:00
+4		NED	ARENSMAN Thymen	INEOS Grenadiers	+ 02:33
+5		COL	BUITRAGO Santiago	Bahrain Victorious	+ 02:52
+6		COL	RUBIO Einer	Movistar Team	+ 03:35
+7		USA	RICCITELLO Matthew	Israel-Premier Tech	+ 04:58
+8		BEL	HUYS Laurens	Intermarché-Circus-Wanty	+ 06:08
+9		ECU	CEPEDA Alexander	EF Education-EasyPost	+ 06:27
+10		BEL	VAN WILDER Ilan	Soudal Quick-Step	+ 06:47
+11		NED	VAN DEN BERG Lars	Groupama-FDJ	+ 07:52
+12		GER	HESSMAN Michel	Jumbo-Visma	+ 09:01
+13		KAZ	PRONSKIY Vadim	Astana Qazaqstan Team	+ 09:20
+14		USA	MCNULTY Brandon	UAE Team Emirates	+ 09:47
+15		ITA	ZANA Filippo	Team Jayco-AlUla	+ 10:32
+16		ITA	FRIGO Marco	Israel-Premier Tech	+ 10:33
+17		IRL	HEALY Ben	EF Education-EasyPost	+ 12:27
+18		ITA	OLDANI Stefano	Alpecin-Deceuninck	+ 14:08
+19		ITA	VERRE Alessandro	Team Arkéa-Samsic	+ 14:48
+20		ITA	ZAMBANINI Edoardo	Bahrain Victorious	+ 16:14
+21		ITA	ALEOTTI Giovanni	BORA-hansgrohe	+ 16:40
+22		BEL	REX Laurenz	Intermarché-Circus-Wanty	+ 16:57
+23		FRA	PARET-PEINTRE Valentin	AG2R Citroën Team	+ 17:03
+24		ITA	BATTISTELLA Samuele	Astana Qazaqstan Team	+ 17:35
+25		ITA	BAIS Davide	EOLO-Kometa Cycling Team	+ 18:03
+26		AUS	GROVES Kaden	Alpecin-Deceuninck	+ 18:23
+27		ITA	MARCELLUSI Martin	Green Project-Bardiani CSF-Faizanè	+ 19:27
+28		AUS	BERWICK Sebastian	Israel-Premier Tech	+ 23:58
+29		GBR	STEWART Jake	Groupama-FDJ	+ 24:22
+30		GER	MAYRHOFER Marius	Team DSM	+ 25:39
+31		ERI	MULUBRHAN Henok	Green Project-Bardiani CSF-Faizanè	+ 25:58
+32		FRA	BAUDIN Alex	AG2R Citroën Team	+ 29:42
+33		GER	MÄRKL Niklas	Team DSM	+ 30:46
+34		ITA	DAINESE Alberto	Team DSM	+ 31:13
+35		LUX	RIES Michel	Team Arkéa-Samsic	+ 31:20
+36		ITA	ZOCCARATO Samuele	Green Project-Bardiani CSF-Faizanè	+ 32:03
+37		ERI	TESFAZION Natnael	Trek-Segafredo	+ 32:22
+38		HUN	FETTER Erik	EOLO-Kometa Cycling Team	+ 33:20
+39		FRA	CHAMPION Thomas	Cofidis	+ 36:29
+40		ITA	COVI Alessandro	UAE Team Emirates	+ 36:41
+41		ITA	MAGLI Filippo	Green Project-Bardiani CSF-Faizanè	+ 40:28
+42		ITA	MILAN Jonathan	Bahrain Victorious	+ 41:03
+43		GBR	QUARTERMAN Charlie	Team corratec - Selle Italia	+ 41:45
+44		NZL	STEWART Campbell	Team Jayco-AlUla	+ 42:12
+45		ITA	KONYCHEV Alexander	Team corratec - Selle Italia	+ 42:21
+46		FRA	TOUMIRE Hugo	Cofidis	+ 42:41
+47		BEL	MARIT Arne	Intermarché-Circus-Wanty	+ 42:42
+48		NED	DEKKER David	Team Arkéa-Samsic	+ 43:36
+49		CZE	VACEK Karel	Team corratec - Selle Italia	+ 44:40
+50		ITA	IACCHI Alessandro	Team corratec - Selle Italia	+ 44:52
+51		NOR	HVIDEBERG Jonas Iversby	Team DSM	+ 45:23
+52		NED	HOOLE Daan	Trek-Segafredo	+ 47:44
+53		SBA	STOJNIC Veljko	Team corratec - Selle Italia	+ 50:49
+54		GBR	GLOAG Thomas	Jumbo-Visma	+ 51:40"""
 
-blue = """"""
+blue = """1		FRA	PINOT Thibaut	Groupama-FDJ	40
+2		ERI	GEBREIGZABHIER Amanuel	Trek-Segafredo	26
+3		FRA	PARET-PEINTRE Aurélien	AG2R Citroën Team	22
+4		ITA	ZOCCARATO Samuele	Green Project-Bardiani CSF-Faizanè	13
+5		COL	BUITRAGO Santiago	Bahrain Victorious	12
+6		NOR	LEKNESSUND Andreas	Team DSM	10
+7		LAT	SKUJINS Toms	Trek-Segafredo	10
+8		FRA	CHAMPION Thomas	Cofidis	9
+9		ITA	ALBANESE Vincenzo	EOLO-Kometa Cycling Team	8
+10		FRA	BARGUIL Warren	Team Arkéa-Samsic	8
+11		USA	KUSS Sepp	Jumbo-Visma	4
+12		ITA	GANDIN Stefano	Team corratec - Selle Italia	4
+13		GBR	GEOGHEGAN HART Tao	INEOS Grenadiers	3
+14		POR	ALMEIDA Joao	UAE Team Emirates	2
+15		BEL	DE PLUS Laurens	INEOS Grenadiers	2
+16		ITA	CONCI Nicola	Alpecin-Deceuninck	2
+17		AUS	SCOTSON Callum	Team Jayco-AlUla	2
+18		ITA	BAIS Mattia	EOLO-Kometa Cycling Team	2
+19		IRL	DUNBAR Edward	Team Jayco-AlUla	1
+20		FRA	SIVAKOV Pavel	INEOS Grenadiers	1
+21		ITA	FRIGO Marco	Israel-Premier Tech	1
+22		GER	MAYRHOFER Marius	Team DSM	1"""
 
-purple = """"""
+purple = """1		ITA	MILAN Jonathan	Bahrain Victorious	92
+2		AUS	GROVES Kaden	Alpecin-Deceuninck	91
+3		DEN	PEDERSEN Mads	Trek-Segafredo	56
+4		AUS	MATTHEWS Michael	Team Jayco-AlUla	48
+5		NED	DEKKER David	Team Arkéa-Samsic	40
+6		FRA	PARET-PEINTRE Aurélien	AG2R Citroën Team	33
+7		ITA	ALBANESE Vincenzo	EOLO-Kometa Cycling Team	28
+8		ITA	GANDIN Stefano	Team corratec - Selle Italia	24
+9		BEL	MARIT Arne	Intermarché-Circus-Wanty	23
+10		NOR	LEKNESSUND Andreas	Team DSM	22
+11		GBR	CAVENDISH Mark	Astana Qazaqstan Team	18
+12		LAT	SKUJINS Toms	Trek-Segafredo	16
+13		ITA	FIORELLI Filippo	Green Project-Bardiani CSF-Faizanè	16
+14		BEL	EVENEPOEL Remco	Soudal Quick-Step	15
+15		ITA	KONYCHEV Alexander	Team corratec - Selle Italia	15
+16		GER	MAYRHOFER Marius	Team DSM	14
+17		ITA	BONIFAZIO Niccolo	Intermarché-Circus-Wanty	14
+18		ITA	DALLA VALLE Nicolas	Team corratec - Selle Italia	14
+19		COL	GAVIRIA Fernando	Movistar Team	14
+20		FRA	CHAMPION Thomas	Cofidis	13
+21		ITA	MAESTRI Mirco	EOLO-Kometa Cycling Team	12
+22		ITA	VENDRAME Andrea	AG2R Citroën Team	12
+23		ITA	GANNA Filippo	INEOS Grenadiers	12
+24		GER	ACKERMANN Pascal	UAE Team Emirates	12
+25		ERI	GEBREIGZABHIER Amanuel	Trek-Segafredo	10
+26		POR	ALMEIDA Joao	UAE Team Emirates	9
+27		SLO	ROGLIC Primoz	Jumbo-Visma	9
+28		ITA	CONCI Nicola	Alpecin-Deceuninck	9
+29		ITA	CONSONNI Simone	Cofidis	9
+30		ITA	BAIS Mattia	EOLO-Kometa Cycling Team	8
+31		SBA	STOJNIC Veljko	Team corratec - Selle Italia	8
+32		GBR	GEOGHEGAN HART Tao	INEOS Grenadiers	7
+33		GBR	STEWART Jake	Groupama-FDJ	7
+34		FRA	BARGUIL Warren	Team Arkéa-Samsic	6
+35		SUI	KÜNG Stefan	Groupama-FDJ	6
+36		ITA	OLDANI Stefano	Alpecin-Deceuninck	6
+37		ITA	ZOCCARATO Samuele	Green Project-Bardiani CSF-Faizanè	6
+38		NOR	BYSTRØM Sven Erik	Intermarché-Circus-Wanty	5
+39		AUS	VINE Jay	UAE Team Emirates	4
+40		NED	BOUWMAN Koen	Jumbo-Visma	4
+41		ITA	CARUSO Damiano	Bahrain Victorious	3
+42		ITA	VELASCO Simone	Astana Qazaqstan Team	3
+43		USA	MCNULTY Brandon	UAE Team Emirates	3
+44		GBR	THOMAS Geraint	INEOS Grenadiers	2
+45		RUS	VLASOV Aleksandr	BORA-hansgrohe	2
+46		IRL	DUNBAR Edward	Team Jayco-AlUla	2
+47		RSA	GIBBONS Ryan	UAE Team Emirates	2
+48		GER	KANTER Max	Movistar Team	2
+49		FRA	ARMIRAIL Bruno	Groupama-FDJ	1
+50		ITA	MARCELLUSI Martin	Green Project-Bardiani CSF-Faizanè	1
+51		LUX	KIRSCH Alex	Trek-Segafredo	1
+52		FRA	RUSSO Clément	Team Arkéa-Samsic	1"""
 
-scriptresults = """"""
+scriptresults = """kyle - pink - ROGLIC Primoz - 5    
+kyle - pink - EVENEPOEL Remco - 2  
+kyle - pink - CARTHY Hugh - 20     
+kyle - pink - ALMEIDA Joao - 4     
+kyle - pink - THOMAS Geraint - 6   
+kyle - white - EVENEPOEL Remco - 2 
+kyle - white - ALMEIDA Joao - 3    
+kyle - white - MCNULTY Brandon - 14
+kyle - white - ARENSMAN Thymen - 4 
+kyle - white - HUYS Laurens - 8
+kyle - blue - VINE Jay - 23
+kyle - blue - PINOT Thibaut - 1
+kyle - blue - BOUWMAN Koen - 23
+kyle - blue - DOMBROWSKI Joe - 23
+kyle - blue - BARGUIL Warren - 10
+kyle - purple - GAVIRIA Fernando - 19
+kyle - purple - MATTHEWS Michael - 4
+kyle - purple - ACKERMANN Pascal - 24
+kyle - purple - PEDERSEN Mads - 3
+kyle - purple - CAVENDISH Mark - 11
+kyle - 209
+lee - pink - ROGLIC Primoz - 5
+lee - pink - EVENEPOEL Remco - 2
+lee - pink - THOMAS Geraint - 6
+lee - pink - ALMEIDA Joao - 4
+lee - pink - PINOT Thibaut - 17
+lee - white - EVENEPOEL Remco - 2
+lee - white - ALMEIDA Joao - 3
+lee - white - TOUMIRE Hugo - 46
+lee - white - MCNULTY Brandon - 14
+lee - white - HEALY Ben - 17
+lee - blue - PINOT Thibaut - 1
+lee - blue - KUSS, Sepp - 23
+lee - blue - ROGLIČ Primož - 23
+lee - blue - EVENEPOEL Remco - 23
+lee - blue - HART Tao Geoghegan - 23
+lee - purple - CAVENDISH Mark - 11
+lee - purple - PEDERSEN Mads - 3
+lee - purple - MATTHEWS Michael - 4
+lee - purple - CORT Magnus - 53
+lee - purple - EVENEPOEL Remco - 14
+lee - 294
+aaron - pink - EVENEPOEL Remco - 2
+aaron - pink - CARTHY Hugh - 20
+aaron - pink - ROGLIC Primoz - 5
+aaron - pink - ALMEIDA Joao - 4
+aaron - pink - HAIG Jack - 15
+aaron - white - EVENEPOEL Remco - 2
+aaron - white - ALMEIDA Joao - 3
+aaron - white - HEALY Ben - 17
+aaron - white - DAINESE Alberto - 34
+aaron - white - MCNULTY Brandon - 14
+aaron - blue - BOUWMAN Koen - 23
+aaron - blue - KÄMNA Lennard - 23
+aaron - blue - DOMBROWSKI Joe - 23
+aaron - blue - VENDRAME Andrea - 23
+aaron - blue - BARGUIL Warren - 10
+aaron - purple - MATTHEWS Michael - 4
+aaron - purple - CORT Magnus - 53
+aaron - purple - PEDERSEN Mads - 3
+aaron - purple - ACKERMANN Pascal - 24
+aaron - purple - GROVES Kaden - 2
+aaron had GROVES Kaden
+aaron - 299"""
